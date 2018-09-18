41b042c3a07ee199a512714beaf2cef508531f14
vicecarloans <huyst1997@icloud.com>
2018-09-18 00:52:17 -0400

    New Commit For Deployment

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production'){
+    // Express will serve up production assets, main.js, main.css
+    app.use(express.static('client/build'));
+    
+    // Express will serve index.html if route not recognized
+    //Order of operation
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
