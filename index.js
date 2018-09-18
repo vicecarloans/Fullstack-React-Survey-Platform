2117ddf7722d55d9b39ae191c73a94ce83be306c
vicecarloans <huyst1997@icloud.com>
2018-09-18 01:06:50 -0400

    Fix it again

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'production'){
     //Order of operation
     const path = require('path')
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+        res.sendFile('index.html');
     });
 }
 
